e37ee46f0a0a64b4e6fd3c8b3d2ecb1ebc102aee
MaxMV8 <alu19_164@365.stucom.com>
2024-10-03 17:45:20 +0200

    Update multiples-de-ambos.py
    actualizado a FizzBuzz

--- multiples-de-ambos.py
+++ multiples-de-ambos.py
@@ -1,7 +1,7 @@
 lista = []
 for numero in range(1, 101):
     if numero % 3 == 0 and numero %5 == 0:
-        lista.append("Coca")
+        lista.append("FizzBuzz")
     else:
         lista.append(numero)
 print (lista)
